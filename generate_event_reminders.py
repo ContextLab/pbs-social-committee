24feabaf8049b65286e8f3c13666d3a2c424cea6
Jeremy R. Manning <jeremymanning@users.noreply.github.com>
2024-07-18 14:34:35 -0400

    Update generate_event_reminders.py

--- generate_event_reminders.py
+++ generate_event_reminders.py
@@ -32,10 +32,10 @@ def create_event_script(event_name, date_str, content_file, frequency, day_of_we
     with open(f'templates/{content_file}', 'r') as file:
         email_content = file.read()
 
-    email_content = email_content.replace('{{DATE}}', date + '\n\n').replace('{{TIME}}', time + '\n\n').replace('{{LOCATION}}', location)
+    email_content = email_content.replace('{{DATE}}', date).replace('{{TIME}}', time).replace('{{LOCATION}}', location)
 
     # Insert event content into admin template
-    full_content = admin_template.replace('===BEGIN===', '===BEGIN===\n\n' + email_content)
+    full_content = admin_template.replace('===BEGIN===', '===BEGIN===\n' + email_content + '\n').replace('===END===', '\n===END===')
 
     # Convert the full email content to HTML
     full_content_html = markdown.markdown(full_content)
